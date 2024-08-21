26b09607afa0fcc6c8b3138604e71394ac606255
yamaki <yamaki2309@gmail.com>
2024-08-21 10:28:08 +0900

    testtest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,43 +14,43 @@ class PostController extends Controller
         return view('post.index', compact('posts'));
     }
 
-    public function create()
+    public function create()//home page
     {
         return view('post.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request)//saving
     {
         $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'body' => 'required|string',
         ]);
 
-        $post = new Post();
+        $post = new Post();//select model(database)
         $post->title = $validatedData['title'];
         $post->body = $validatedData['body'];
         $post->user_id = Auth::id();
-        $post->save();
+        $post->save();//save to database
 
         return redirect()->route('post.index')->with('success', '投稿が作成されました');
     }
 
-    public function myPosts()
+    public function myPosts()//read my post
     {
-        $posts = Post::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->get();
+        $posts = Post::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->get();// where: condition
         return view('my-posts', compact('posts'));
     }
 
-    public function edit($id)
+    public function edit($id)//edit my post
     {
         $post = Post::findOrFail($id);
         return view('post.edit', compact('post'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id)//update 
     {
         $validatedData = $request->validate([
-            'title' => 'required|string|max:255',
+            'title' => 'required|string|max:255',//255: max litereture
             'body' => 'required|string',
         ]);
 
